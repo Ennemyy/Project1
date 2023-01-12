41ab278259335ad9f0fd633af9a3886e81ad28b8
Ennemyy <bennyawm@gmail.com>
2023-01-12 10:01:12 -0500

    prueba

---
+++ prueba.js
@@ -0,0 +1,10 @@
+const minombre = "Sebastian";
+const edad = 19;
+const desarrollador = true;
+const fecha_de_nacimiento= new Date("24-07-2003");
+const libro_favorito= {
+    titulo : "La Vaca",
+    autor : "Camilo Cruz",
+    fecha : new Date("03-01-2003"),
+    url : "http://courseware.url.edu.gt/Facultades/Facultad%20de%20Teolog%C3%ADa/Magis%20Landivariano%20Material/Libros/Libro%20La%20VACA.pdf"
+};
